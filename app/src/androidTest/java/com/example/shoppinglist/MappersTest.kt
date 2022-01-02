1268afb334410bbef2f71b875756f872706764f2
DESKTOP-Mallorax <patryk733773@gmail.com>
2022-01-02 19:42:30 +0100

    Added tests for data mappers

---
+++ app/src/androidTest/java/com/example/shoppinglist/MappersTest.kt
@@ -0,0 +1,66 @@
+package com.example.shoppinglist
+
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.shoppinglist.model.appmodel.Grocery
+import com.example.shoppinglist.model.appmodel.ShoppingList
+import com.example.shoppinglist.model.appmodel.ShoppingListStatus
+import com.example.shoppinglist.model.dbmodel.GroceryEntity
+import com.example.shoppinglist.model.dbmodel.ShoppingListEntity
+import com.example.shoppinglist.repository.*
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class MappersTest {
+
+    @Test
+    fun mapStringToShoppingListStatusTest(){
+        val statusString = "archived"
+        val status = mapStringToShoppingListStatus(statusString)
+        assertThat(status.status, equalTo(ShoppingListStatus.ARCHIVED.status))
+    }
+
+    @Test
+    fun mapShoppingListEntityToShoppingListTest(){
+        val shoppingListEntity = ShoppingListEntity("test", 1L, "current", 1L)
+        val shoppingList = mapShoppingListEntityToShoppingList(shoppingListEntity)
+        assertThat(shoppingListEntity.listName, equalTo(shoppingList.listName))
+        assertThat(shoppingListEntity.date, equalTo(shoppingList.date.timeInMillis))
+        assertThat(mapStringToShoppingListStatus(shoppingListEntity.status), equalTo(shoppingList.status))
+        assertThat(shoppingListEntity.listId, equalTo(shoppingList.creation))
+    }
+
+    @Test
+    fun mapShoppingListToShoppingListEntityTest(){
+        val shoppingList = ShoppingList(Calendar.getInstance(), "test", ShoppingListStatus.CURRENT, 1L)
+        val shoppingListEntity = mapShoppingListToShoppingListEntity(shoppingList)
+        assertThat(shoppingListEntity.listName, equalTo(shoppingList.listName))
+        assertThat(shoppingListEntity.date, equalTo(shoppingList.date.timeInMillis))
+        assertThat(mapStringToShoppingListStatus(shoppingListEntity.status), equalTo(shoppingList.status))
+        assertThat(shoppingListEntity.listId, equalTo(shoppingList.creation))
+    }
+
+    @Test
+    fun mapDbGroceryToAppGroceryTest(){
+        val groceryEntity = GroceryEntity("name", 1, 1L, 1L)
+        val grocery = mapDbGroceryToAppGrocery(groceryEntity)
+        assertThat(groceryEntity.name, equalTo(grocery.name))
+        assertThat(groceryEntity.amount, equalTo(grocery.amount))
+        assertThat(groceryEntity.listFkId, equalTo(grocery.shoppingListId))
+        assertThat(groceryEntity.groceryId, equalTo(grocery.groceryId))
+    }
+
+    @Test
+    fun mapAppGroceryToDbGroceryTest(){
+        val grocery = Grocery("test", 1, 1L, 1L)
+        val groceryEntity = mapAppGroceryToDbGrocery(grocery)
+        assertThat(groceryEntity.name, equalTo(grocery.name))
+        assertThat(groceryEntity.amount, equalTo(grocery.amount))
+        assertThat(groceryEntity.listFkId, equalTo(grocery.shoppingListId))
+        assertThat(groceryEntity.groceryId, equalTo(grocery.groceryId))
+    }
+
+}
