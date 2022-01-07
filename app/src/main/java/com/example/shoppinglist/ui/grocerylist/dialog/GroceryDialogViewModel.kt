a7ff6f07c30fd2bbe2b40aa2c9224d95bf9295a2
DESKTOP-Mallorax <patryk733773@gmail.com>
2022-01-07 13:25:25 +0100

    Adding LiveEvents

--- app/src/main/java/com/example/shoppinglist/ui/grocerylist/dialog/GroceryDialogViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/ui/grocerylist/dialog/GroceryDialogViewModel.kt
@@ -1,10 +1,11 @@
 package com.example.shoppinglist.ui.grocerylist.dialog
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.shoppinglist.model.appmodel.Grocery
 import com.example.shoppinglist.repository.ShoppingListRepositoryImpl
-import com.example.shoppinglist.ui.SingleLiveEvent
+import com.hadilq.liveevent.LiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,16 +15,19 @@ import javax.inject.Inject
 @HiltViewModel
 class GroceryDialogViewModel @Inject constructor(private val repositoryImpl: ShoppingListRepositoryImpl): ViewModel() {
 
-    val errorMessage = SingleLiveEvent<String>()
-    val dismissDialog = SingleLiveEvent<Boolean>()
+    private val _errorMessage = LiveEvent<String>()
+    val errorMessage: LiveData<String> get() = _errorMessage
+
+    private val _dismissDialog = LiveEvent<Boolean>()
+    val dismissDialog: LiveData<Boolean> get() = _dismissDialog
 
     fun addGrocery(name: String, quantity: String, shoppingListId: Long) {
         try {
             val grocery = Grocery(name, Integer.parseInt(quantity), shoppingListId, Calendar.getInstance().timeInMillis)
             saveGrocery(grocery)
-            dismissDialog.value = true
+            _dismissDialog.value = true
         } catch (e: NumberFormatException) {
-            errorMessage.value = "Put a number in quantity field"
+            _errorMessage.value = "Put a number in quantity field"
         }
     }
 
